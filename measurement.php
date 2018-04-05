ee92ee03f04378ac737610e5d2a12e1dc6a6578b
PD5WL <pd5wl@dkars.nl>
2018-04-05 16:56:17 +0200

    Meteting toegevoegd.

---
+++ measurement.php
@@ -0,0 +1,59 @@
+<?php
+// Connect
+
+
+include ('./head.html');
+include ('./header.html');
+
+include './dbconn.php';
+
+// Run Query
+$sql 	= 'SELECT * FROM Measurement WHERE DevID IN (SELECT DevID FROM Measurement WHERE TimestampUTC = (SELECT MAX(TimestampUTC) FROM Measurement)) ORDER BY DevID DESC LIMIT 1';
+$stmt 	= $pdo->prepare($sql); // Prevent MySQl injection. $stmt means statement
+$stmt->execute();
+?>
+<!-- Tabel opmaak -->
+
+<table cellspacing="2">
+  <tbody>
+    <tr>
+      <th scope="col" width="80" align="left" style="background-color: beige">Temperatuur</th>
+      <th scope="col" width="100" align="right" style="background-color: beige">Luchtvochtigheid</th>
+      <th scope="col" width="100" align="left" style="background-color: beige">Luchtdruk</th>
+      <th scope="col" width="100" align="left" style="background-color: beige">Spanning</th>
+          </tr> 
+<?php
+while ($row = $stmt->fetch())
+{
+	echo '<tr><td align="center">';
+	echo $row['Temperature'];
+	echo '</td><td align="left">';
+	echo $row['Humidity'];
+	echo '</td><td align="left">';
+	echo $row['Pressure'];
+	echo '</td><td align="left">';
+	echo $row['Batt'];
+	echo '</td></tr>';
+
+}
+?>
+  </tbody>
+</table>
+<?php
+$date = date_create();
+$tijd =  date_format($date, 'Y-m-d H:i:s');
+
+	echo '<br />';
+	echo 'Lijst gemaakt op: ';
+	echo $tijd;
+	echo '<br />';
+
+// Close connection
+$pdo = null;
+// Show disconnect
+//	echo '<br />';
+//	echo 'Closed connection to MySQL';
+//	echo '<br />';
+
+include ('./footer.html');
+?>
