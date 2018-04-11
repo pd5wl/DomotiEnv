6336c6b81de946a4cba6b21210c7887fd501930f
Wijnand Laros <pd5wl@dkars.nl>
2018-04-11 11:53:47 +0200

    Create config.php
    Basic config file

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	Configfile
+*/
+//Database
+
+	// database server hostname
+	$servername = "[servername]";
+	// database table
+	$dbname = "[db_name]";
+	// database user name
+	$username = "[username]";
+	// database password
+	$password = "[userpass]";
+
+// Setup
+
+	// Enable setup 1 = Enable / 0 = Disable
+	$setup = 1;
+?>
