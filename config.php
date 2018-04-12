f8bccec12d274faa8890000538c2252bc0ebf831
Wijnand Laros <pd5wl@dkars.nl>
2018-04-12 23:05:38 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	Configfile
+*/
+//Database
+	// database server hostname
+	$servername = "[server]";
+	// database table
+	$dbname = "[ddname]";
+	// database user name
+	$username = "[username]";
+	// database password
+	$password = "[password]";
+// Setup
+	// Enable setup 1 = Enable / 0 = Disable
+	$setup = 0;
+?>
