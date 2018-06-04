6b1da0efd102c2942b78b6116be021570c2242fd
PD5WL <pd5wl@dkars.nl>
2018-06-04 22:01:12 +0200

    More floor plan stuff

---
+++ output/balkon.php
@@ -0,0 +1,82 @@
+<h2>Balkon</h2>
+<table width="60%" border="0" align="center">
+  <tbody>
+<?php
+try {
+
+ $sql = 'SELECT 
+    m1.DevID AS DevID, 
+    m1.TimestampUTC AS TimestampUTC, 
+    m1.Temperature AS Temperature,
+    m1.Humidity AS Humidity,
+    m1.Pressure AS Pressure,
+    m1.Batt AS Batt,
+    n.DevOmschr AS DevOmschr
+    
+FROM
+    Measurement m1 
+INNER JOIN
+    (
+    SELECT
+        mi.DevID AS DevID,
+        MAX(mi.TimestampUTC) AS maxtimestamp
+    FROM
+        Measurement mi
+    GROUP BY
+        mi.DevID
+) m2
+ON
+    (
+        m1.TimestampUTC = m2.maxtimestamp AND m1.DevID = m2.DevID
+    ) 
+INNER JOIN
+    Node n
+ON
+    n.DevID = m1.DevID
+GROUP BY m1.DevID';
+	
+$stmt 	= $pdo->prepare($sql); // Prevent MySQl injection. $stmt means statement
+$stmt->execute();
+}
+catch(PDOException $e)
+{
+echo $e->getMessage();
+}
+
+while ($row = $stmt->fetch())
+{
+	echo '<tr><td align="left">';
+	echo 'Temperatuur';
+    echo '</td><td align="right">';
+	echo $row['Temperature'];
+	echo '&deg;C';
+	echo '</td></tr>';
+	echo '<tr><td align="left">';
+	echo 'Luchtvochtigheid';
+	echo '</td><td align="right">';
+	echo $row['Humidity'];
+	echo '%';
+	echo '</td></tr>';
+	echo '<tr><td align="left">';
+	echo 'Luchtdruk';
+	echo '</td><td align="right">';
+	echo $row['Pressure'];
+	echo 'mbar';
+	echo '</td></tr>';
+	echo '<tr><td align="left">';
+	echo 'Spanning';
+	echo '</td><td align="right">';
+	echo $row['Batt'];
+	echo 'V';
+	echo '</td></tr>';
+	echo '<tr><td align="left">';
+	echo 'Tijd stip meting';
+	echo '</td><td align="right">';
+	echo $row['TimestampUTC'];
+	echo '</td></tr>';
+
+}
+?>
+
+  </tbody>
+</table>
--- output/floorplan.php
+++ output/floorplan.php
@@ -16,72 +16,12 @@ include ('../includes/dbconn.php');
 	<a class="interactive-point" style="left: 51%; top: 86.5%;" data-reveal-id="Meterkast">+</a> 
 	</div>
 </div>
-<!-- PRODUCT -->
-<div id="Balkon" class="reveal-modal"><h2>Balkon</h2>
-
-<?php
-try {
-
- $sql = 'SELECT 
-    m1.DevID AS DevID, 
-    m1.TimestampUTC AS TimestampUTC, 
-    m1.Temperature AS Temperature,
-    m1.Humidity AS Humidity,
-    m1.Pressure AS Pressure,
-    m1.Batt AS Batt,
-    n.DevOmschr AS DevOmschr
-    
-FROM
-    Measurement m1 
-INNER JOIN
-    (
-    SELECT
-        mi.DevID AS DevID,
-        MAX(mi.TimestampUTC) AS maxtimestamp
-    FROM
-        Measurement mi
-    GROUP BY
-        mi.DevID
-) m2
-ON
-    (
-        m1.TimestampUTC = m2.maxtimestamp AND m1.DevID = m2.DevID
-    ) 
-INNER JOIN
-    Node n
-ON
-    n.DevID = m1.DevID
-GROUP BY m1.DevID';
-	
-$stmt 	= $pdo->prepare($sql); // Prevent MySQl injection. $stmt means statement
-$stmt->execute();
-}
-catch(PDOException $e)
-{
-echo $e->getMessage();
-}
-
-while ($row = $stmt->fetch())
-{
-	echo '<br />Temperatuur:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	echo $row['Temperature'];
-	echo '&deg;C';
-	echo '<br />Luchtvochtigheid:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	echo $row['Humidity'];
-	echo '%';
-	echo '<br />Luchtdruk:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	echo $row['Pressure'];
-	echo 'mbar';
-	echo '<br />Spanning:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	echo $row['Batt'];
-	echo 'V';
-	echo '<br />Tijd stip meting:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	echo $row['TimestampUTC'];
-	echo '<br />';
-
-}
-?>	
-<a class="close-reveal-modal">&times;</a></div>
+<!-- Balkon -->
+<div id="Balkon" class="reveal-modal">
+	<?php
+	include('balkon.php')
+	?>
+	<a class="close-reveal-modal">&times;</a></div>
 <!-- PRODUCT -->
 <div id="Woonkamer" class="reveal-modal">Woonkamer<a class="close-reveal-modal">&times;</a></div>
 <!-- PRODUCT -->
